ee7a681f48f1ebce37ba434d41fc1653c1c430c0
Davi Jose Batista Lima De Oliveira <davijosebatistalimadeoliveira@gmail.com>
2022-09-30 20:46:38 -0300

    Verdadeiro Falso concluido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int n1, n2;
+
+Console.WriteLine("divisao de dois numeros");
+
+
+Console.WriteLine("digite seu primeriro numero");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite seu segundo numero");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (n2==0)
+
+{Console.WriteLine("nao é possivel dividir por zero");}
+
+else
+{double resultado = n1/n2;}
